a7efa530bacb413388a8d9136818e3f2e6df48fc
DipaWidia <wiartha2001@gmail.com>
2021-12-23 12:39:54 +0800

    add login regis api

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,12 +5,14 @@ namespace App\Models;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use HasApiTokens, HasFactory, Notifiable;
+    protected $primaryKey = 'id_user';
 
     /**
      * The attributes that are mass assignable.
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ChordController;
 use App\Http\Controllers\Api\CommentController;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,23 +17,63 @@ use App\Http\Controllers\Api\CommentController;
 |
 */
 
-Route::get('chords', [ChordController::class, 'index']);
-Route::post('chords/create', [ChordController::class, 'create']);
-Route::post('chords/update', [ChordController::class, 'update']);
-Route::post('chords/delete', [ChordController::class, 'delete']);
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/refresh', [AuthController::class, 'refresh']);
+    Route::get('/user-profile', [AuthController::class, 'userProfile']);    
+
+    Route::get('chords', [ChordController::class, 'index']);
+    Route::post('chords/create', [ChordController::class, 'create']);
+    Route::post('chords/update', [ChordController::class, 'update']);
+    Route::post('chords/delete', [ChordController::class, 'delete']);
+    
+    Route::post('comments', [CommentController::class, 'index']);
+    Route::post('comments/create', [CommentController::class, 'create']);
+
+    //untuk ngambil semua chord dari user tertentu, parameter: id_user
+    Route::post('chords/user', [ChordController::class, 'indexUser']);
+
+    //untuk filter by genre, parameter: genre
+    Route::post('chords/genre', [ChordController::class, 'indexGenre']);
+
+    //untuk filter by level, parameter: level
+    Route::post('chords/level', [ChordController::class, 'indexLevel']);
+
+    //untuk search, parameter: keyword
+    Route::post('chords/search', [ChordController::class, 'indexSearch']);
+});
+// Route::post('register', 'App\Http\Controllers\Api\AuthController@insertPengguna');
+// Route::post('login', 'App\Http\Controllers\Api\AuthController@login');
+
+// Route::post('register', 'AuthController@insertPengguna');
+
+// Route::get('chords', [ChordController::class, 'index']);
+// Route::post('chords/create', [ChordController::class, 'create']);
+// Route::post('chords/update', [ChordController::class, 'update']);
+// Route::post('chords/delete', [ChordController::class, 'delete']);
 
 
-Route::post('comments', [CommentController::class, 'index']);
-Route::post('comments/create', [CommentController::class, 'create']);
+// Route::post('comments', [CommentController::class, 'index']);
+// Route::post('comments/create', [CommentController::class, 'create']);
 
-//untuk ngambil semua chord dari user tertentu, parameter: id_user
-Route::post('chords/user', [ChordController::class, 'indexUser']);
+// //untuk ngambil semua chord dari user tertentu, parameter: id_user
+// Route::post('chords/user', [ChordController::class, 'indexUser']);
 
-//untuk filter by genre, parameter: genre
-Route::post('chords/genre', [ChordController::class, 'indexGenre']);
+// //untuk filter by genre, parameter: genre
+// Route::post('chords/genre', [ChordController::class, 'indexGenre']);
 
-//untuk filter by level, parameter: level
-Route::post('chords/level', [ChordController::class, 'indexLevel']);
+// //untuk filter by level, parameter: level
+// Route::post('chords/level', [ChordController::class, 'indexLevel']);
 
-//untuk search, parameter: keyword
-Route::post('chords/search', [ChordController::class, 'indexSearch']);
+// //untuk search, parameter: keyword
+// Route::post('chords/search', [ChordController::class, 'indexSearch']);
