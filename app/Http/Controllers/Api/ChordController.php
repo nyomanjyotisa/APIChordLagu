b136d5f9839646d9367f910e6f203346f38fa476
nyomanjyotisa <jyotisa1616@gmail.com>
2021-12-25 09:44:36 +0800

    my last commit

--- app/Http/Controllers/Api/ChordController.php
+++ app/Http/Controllers/Api/ChordController.php
@@ -12,29 +12,6 @@ class ChordController extends Controller
         $chords = Chord::all();
 
         return $chords;
-        // return response()->json([
-        //     // 'success'=>true,
-        //     'data'=>$chords
-        // ]);
-    }
-
-    public function create(Request $request){
-        $chord = new Chord;
-        $chord->id_user = 1;
-        $chord->judul = $request->judul;
-        $chord->penyanyi = $request->penyanyi;
-        $chord->level = $request->level;
-        $chord->genre = $request->genre;
-        $chord->durasi_menit = $request->durasi_menit;
-        $chord->durasi_detik = $request->durasi_detik;
-        $chord->chord_dan_lirik = $request->chord_dan_lirik;
-        $chord->save();
-
-        return response()->json([
-            'success'=>true,
-            'message'=>'Berhasil Menambahkan Data Chord',
-            'data' => $chord,
-        ]);
     }
 
     public function store(Request $request){
@@ -57,64 +34,8 @@ class ChordController extends Controller
         ]);
     }
 
-    public function update(Request $request){
-        $chord = Chord::find($request->id);
-        $chord->judul = $request->judul;
-        $chord->penyanyi = $request->penyanyi;
-        $chord->level = $request->level;
-        $chord->genre = $request->genre;
-        $chord->durasi_menit = $request->durasi_menit;
-        $chord->durasi_detik = $request->durasi_detik;
-        $chord->chord_dan_lirik = $request->chord_dan_lirik;
-        $chord->update();
-
-        return response()->json([
-            'success'=>true,
-            'message'=>'Berhasil Mengedit Data Chord',
-            'data' => $chord,
-        ]);
-    }
-
-    public function delete(Request $request){
-        $chord = Chord::find($request->id);
-        $chord->delete();
-        return response()->json([
-            'success'=>true,
-            'message'=>'Berhasil Menghapus Data Chord',
-        ]);
-    }
-
     public function indexUser(Request $request){
         $chords = Chord::where('id_user', $request->id_user)->get();
-
-        return response()->json([
-            'success'=>true,
-            'data'=>$chords
-        ]);
-    }
-
-    public function indexGenre(Request $request){
-        $chords = Chord::where('genre', 'LIKE', '%'.$request->genre.'%')->get();
-
-        return response()->json([
-            'success'=>true,
-            'data'=>$chords
-        ]);
-    }
-
-    public function indexLevel(Request $request){
-        $chords = Chord::where('level', $request->level)->get();
-
-        return response()->json([
-            'success'=>true,
-            'data'=>$chords
-        ]);
-    }
-
-    public function indexSearch(Request $request){
-        $chords = Chord::where('judul', 'LIKE', '%'.$request->keyword.'%')
-                        ->orWhere('penyanyi', 'LIKE', '%'.$request->keyword.'%')
-                        ->get();
 
         return response()->json([
             'success'=>true,
--- routes/api.php
+++ routes/api.php
@@ -18,10 +18,6 @@ use App\Models\Chord;
 |
 */
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
-
     Route::get('user/{id}/detail', [AuthController::class, 'getPengguna']);
     Route::post('user/login', [AuthController::class, 'login']);
     Route::post('user/register', [AuthController::class, 'register']);
@@ -30,57 +26,15 @@ use App\Models\Chord;
     Route::get('user/user-profile', [AuthController::class, 'userProfile']);    
 
     Route::get('chords', [ChordController::class, 'index']);
-    Route::post('chords/create', [ChordController::class, 'create']);
     Route::post('chords/store', [ChordController::class, 'store']);
-    Route::post('chords/update', [ChordController::class, 'update']);
-    Route::post('chords/delete', [ChordController::class, 'delete']);
     Route::post('chords/deleteuser', [ChordController::class, 'deleteUser']);
+    Route::post('chords/user', [ChordController::class, 'indexUser']);
     
     Route::post('comments', [CommentController::class, 'index']);
     Route::post('comments/create', [CommentController::class, 'create']);
-    Route::post('comments/store', [CommentController::class, 'store']);
-    Route::post('comments/user', [CommentController::class, 'indexUser']);
-
-    //untuk ngambil semua chord dari user tertentu, parameter: id_user
-    Route::post('chords/user', [ChordController::class, 'indexUser']);
-
-    //untuk filter by genre, parameter: genre
-    Route::post('chords/genre', [ChordController::class, 'indexGenre']);
-
-    //untuk filter by level, parameter: level
-    Route::post('chords/level', [ChordController::class, 'indexLevel']);
-
-    //untuk search, parameter: keyword
-    Route::post('chords/search', [ChordController::class, 'indexSearch']);
 
     Route::get('chords/{judul}',function($judul){
         $judul = isset($judul) ? trim($judul) : false;
         $query=Chord::where('judul', 'LIKE', '%'.$judul. '%')->get();
         return $query;
     })->where('judul', '^[a-zA-Z ]*$');
-
-// Route::post('register', 'App\Http\Controllers\Api\AuthController@insertPengguna');
-// Route::post('login', 'App\Http\Controllers\Api\AuthController@login');
-
-// Route::post('register', 'AuthController@insertPengguna');
-
-// Route::get('chords', [ChordController::class, 'index']);
-// Route::post('chords/create', [ChordController::class, 'create']);
-// Route::post('chords/update', [ChordController::class, 'update']);
-// Route::post('chords/delete', [ChordController::class, 'delete']);
-
-
-// Route::post('comments', [CommentController::class, 'index']);
-// Route::post('comments/create', [CommentController::class, 'create']);
-
-// //untuk ngambil semua chord dari user tertentu, parameter: id_user
-// Route::post('chords/user', [ChordController::class, 'indexUser']);
-
-// //untuk filter by genre, parameter: genre
-// Route::post('chords/genre', [ChordController::class, 'indexGenre']);
-
-// //untuk filter by level, parameter: level
-// Route::post('chords/level', [ChordController::class, 'indexLevel']);
-
-// //untuk search, parameter: keyword
-// Route::post('chords/search', [ChordController::class, 'indexSearch']);
